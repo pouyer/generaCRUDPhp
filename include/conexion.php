8ca4fea69dfb7a50a317c1a7481ce003b466b3f2
cmejia <66426389+pouyer@users.noreply.github.com>
2024-11-05 09:26:07 -0500

    Update conexion.php

--- include/conexion.php
+++ include/conexion.php
@@ -3,13 +3,10 @@
     // Verificar si se seleccionó una base de datos
     // Configuración de la conexión
     
-/*  $servidor = 'localhost';
+    $servidor = 'localhost';
     $usuario = 'root';
     $password = '';
-*/
-    $servidor = 'srv691.hstgr.io';
-    $usuario = 'u420920295_QuickBite';
-    $password = 'QuickBite1';
+
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $dbname = $_POST['base_datos'];
